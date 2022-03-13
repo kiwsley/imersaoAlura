175ed462cd319bfed785c0b06fccdcd653c0794e
kiwsley <kiwsleyfreire@gmail.com>
2022-03-12 23:49:01 -0300

    calculadorademedia

---
+++ Calculadora de Média/calculadora.js
@@ -0,0 +1,14 @@
+var nome= "kiwsley"
+var notaDoPrimeiroBimestre=9.435;
+var notaDoSegundoBimestre=7.582;
+var notaDoTerceiroBimestre=4.563;
+var notaDoQuartoBimestre=2.823;
+var notaFinal=  (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre )/4;
+var notaFixada=notaFinal.toFixed(1)
+
+console.log("Meu nome é " + nome)
+console.log(notaDoPrimeiroBimestre)
+console.log(notaFixada)
+
+// revisão
+//variáveis, string, console.log, toFixed(), operações matemática, concatenação
